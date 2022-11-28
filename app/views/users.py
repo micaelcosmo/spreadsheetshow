afb98a22a027eb908e7b0ad19c5924c5c84bdb87
Micael Cosmo <micael.trabalho@hotmail.com>
2022-11-28 15:02:53 -0300

    Return JSON
    Método que faz modelo de return em json.

---
+++ app/views/users.py
@@ -0,0 +1,21 @@
+from werkzeug.security import generate_password_hash
+from app import db
+from flask import request, jsonify
+from ..models.users import Users, user_schema, users_schema
+
+
+def post_user():
+    username = request.json['username']
+    password = request.json['password']
+    name = request.json['name']
+    email = request.json['email']
+    pass_hash = generate_password_hash(password)
+    user = Users(username, pass_hash, name, email)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        result = user_schema.dump(user)
+        return jsonify({'message': 'successfully registred', 'data': result.data}), 201
+    except:
+        return jsonify({'message': 'unable to create', 'data': {}}), 500
