913b14954126f15d3ab21956c7050f4214d78a9f
Micael Cosmo <micael.trabalho@hotmail.com>
2022-11-28 09:16:04 -0300

    Primeira Rota
    Página retornando Hello World!

--- __init__.py
+++ __init__.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return jsonify({'message': 'Hello World!'})
 
 
 if __name__ == '__main__':
