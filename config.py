d86b437fee6fac5806fa439ddf43c2c6a3ac269f
Micael Cosmo <micael.trabalho@hotmail.com>
2022-11-29 15:39:00 -0300

    Config
    Novas configurações para autenticação e hash aleatório.

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import random
+import string
+
+
+random_str = string.ascii_letters+string.digits+string.ascii_uppercase
+key = ''.join(random.choice(random_str) for i in range(12))
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/spreadsheetshow'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = key
